0a76442a07ad1238bd57ea0786c4060b42323374
Deepikabisht-student <deepikabisht1312@gmail.com>
2024-02-03 18:49:39 +1300

    fix productdisplay

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -3,6 +3,7 @@ import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
 import { ShopContext } from '../../context/ShopContext'
+//import RelatedProduct from '../RelatedProducts/RelatedProducts'                                                   
 
 const ProductDisplay = (props) => {
     const {product} = props;
